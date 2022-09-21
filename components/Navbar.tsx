266699e89f2a007b128d8a9ad6bac40509efca52
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2022-09-21 14:02:23 +0100

    google oauth login

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { GoogleLogin } from "@react-oauth/google";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
 import { AiOutlineLogout } from "react-icons/ai";
 import { BiSearch } from "react-icons/bi";
 import { IoMdAdd } from "react-icons/io";
@@ -11,7 +11,8 @@ import { createOrGetUser } from "../utils";
 import useAuthStore from "../store/authStore";
 
 const Navbar = () => {
-  const { addUser, userProfile } = useAuthStore();
+  const { addUser, userProfile, removeUser } = useAuthStore();
+  console.log(userProfile);
 
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4 '>
@@ -23,7 +24,38 @@ const Navbar = () => {
       <div>search</div>
       <div>
         {userProfile ? (
-          <div>{userProfile.userName}</div>
+          <div className='flex items-center gap-5 md:gap-10'>
+            <Link href='/upload'>
+              <button className='border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2'>
+                <IoMdAdd className='text-xl md:text-4xl' />
+                <span className='hidden md:block'>upload</span>
+              </button>
+            </Link>
+            {userProfile && (
+              <Link href='/'>
+                <a className=' w-8 h-8'>
+                  <Image
+                    src={userProfile.image}
+                    alt='profile photo'
+                    layout='responsive'
+                    width={62}
+                    height={62}
+                    className='rounded-full'
+                  />
+                </a>
+              </Link>
+            )}
+            <button
+              type='button'
+              className='pr-2'
+              onClick={() => {
+                googleLogout();
+                removeUser();
+              }}
+            >
+              <AiOutlineLogout color='red' fontSize={25} />
+            </button>
+          </div>
         ) : (
           <GoogleLogin
             onSuccess={(credentialResponse) => {
